abeecde838b74b37bc8b510f69485a4e0c417d06
Jorge Cisneros <jorgecis@users.noreply.github.com>
2021-05-03 22:39:37 -0500

    This header don't need the ";"
    https://developers.google.com/speed/pagespeed/insights/ report that the cache is not enable if the ";" is there.

--- add-cache-control-header/index.js
+++ add-cache-control-header/index.js
@@ -3,7 +3,7 @@ function handler(event) {
     var headers = response.headers;
     
     // Set the cache-control header
-     headers['cache-control'] = {value: 'public, max-age=63072000;'};
+     headers['cache-control'] = {value: 'public, max-age=63072000'};
         
     // Return response to viewers
     return response;
