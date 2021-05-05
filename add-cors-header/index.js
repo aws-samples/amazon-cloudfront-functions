e49603960b61b00e43a40eae4a7604ad8c18b1f7
David Brown <brownvok@amazon.com>
2021-05-05 11:05:33 -0700

    fix function to use correct response header.

--- add-cors-header/index.js
+++ add-cors-header/index.js
@@ -5,9 +5,10 @@ function handler(event)  {
     // If Access-Control-Allow-Origin CORS header is missing, add it.
     // Since JavaScript doesn't allow for hyphens in variable names, we use the dict["key"] notation.
     if (!response.headers['access-control-allow-origin'] && request.headers['origin']) {
-        headers['access-control-allow-origin'] = {value: request.headers['origin'].value};
+        response.headers['access-control-allow-origin'] = {value: request.headers['origin'].value};
         console.log("Access-Control-Allow-Origin was missing, adding it now.");
     }
 
     return response;
 }
+
