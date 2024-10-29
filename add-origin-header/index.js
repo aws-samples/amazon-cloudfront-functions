eeccf8b2c4102ac75da7d78e597efc55a57af43f
Liz Kellebrew-Davies <35241259+lkdavies@users.noreply.github.com>
2024-10-29 10:59:57 -0700

    Update index.js

--- add-origin-header/index.js
+++ add-origin-header/index.js
@@ -1,7 +1,7 @@
-function handler(event) {
-    var request = event.request;
-    var headers = request.headers;
-    var host = request.headers.host.value;
+async function handler(event) {
+    const request = event.request;
+    const headers = request.headers;
+    const host = request.headers.host.value;
    
    // If origin header is missing, set it equal to the host header.
    if (!headers.origin)
