fbaba7d7d0fd93658a1a20cf1f2361b5ce5905b5
Liz Kellebrew-Davies <35241259+lkdavies@users.noreply.github.com>
2024-10-29 10:57:35 -0700

    Update index.js

--- add-cache-control-header/index.js
+++ add-cache-control-header/index.js
@@ -1,12 +1,12 @@
-function handler(event) {
-    var response = event.response;
-    var headers = response.headers;
-
+async function handler(event) {
+    const response = event.response;
+    const headers = response.headers;
+    
     if (response.statusCode >= 200 && response.statusCode < 400) {
         // Set the cache-control header
         headers['cache-control'] = {value: 'public, max-age=63072000'};
     }
-
+        
     // Return response to viewers
     return response;
 }
