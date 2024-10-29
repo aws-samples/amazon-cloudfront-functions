5b65b2e496215300049619f6e27b77f0fc8b7903
Liz Kellebrew-Davies <35241259+lkdavies@users.noreply.github.com>
2024-10-29 11:19:17 -0700

    Update index.js

--- redirect-based-on-country/index.js
+++ redirect-based-on-country/index.js
@@ -1,14 +1,14 @@
-function handler(event) {
-    var request = event.request;
-    var headers = request.headers;
-    var host = request.headers.host.value;
-    var country = 'DE' // Choose a country code
-    var newurl = `https://${host}/de/index.html` // Change the redirect URL to your choice 
+async function handler(event) {
+    const request = event.request;
+    const headers = request.headers;
+    const host = request.headers.host.value;
+    const country = Symbol.for('DE'); // Choose a country code
+    const newurl = `https://${host}/de/index.html`; // Change the redirect URL to your choice 
   
     if (headers['cloudfront-viewer-country']) {
-        var countryCode = headers['cloudfront-viewer-country'].value;
+        const countryCode = Symbol.for(headers['cloudfront-viewer-country'].value);
         if (countryCode === country) {
-            var response = {
+            const response = {
                 statusCode: 302,
                 statusDescription: 'Found',
                 headers:
